b0201ab06820ab67f28aa0ce7f98a19a68d0e650
ThanhChung <ntchung18041999@gmail.com>
2020-10-17 20:45:32 +0700

    Updates

---
+++ precache-manifest.83df0c18161bcba08dbf96f9dddc3106.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75b95340b52c1828d30f8001470fa3f3",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "0986bbbeaddb9b9a6c69",
+    "url": "/shopping-cart/static/css/main.62d03321.chunk.css"
+  },
+  {
+    "revision": "ae047f7f72aafe4adb3a",
+    "url": "/shopping-cart/static/js/2.0f3700fe.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/shopping-cart/static/js/2.0f3700fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0986bbbeaddb9b9a6c69",
+    "url": "/shopping-cart/static/js/main.7e37a5e5.chunk.js"
+  },
+  {
+    "revision": "ba80342531975976b290",
+    "url": "/shopping-cart/static/js/runtime-main.6e52efc5.js"
+  }
+]);
